f3f1f535bbcbbc35c43f4aad70cb4d2b51cc84d0
Matthew Kent <matthew.e.kent@gmail.com>
2013-02-07 11:13:35 -0800

    tutorial part 4; more templates and generic views

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,19 @@
 # Create your views here.
-from django.http import HttpResponse, Http404
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
-from polls.models import Poll
-
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
-
-def detail(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', {'poll': p})
-
-def results(request, poll_id):
-    return HttpResponse("you're looking at the results of poll %s" % poll_id)
+from django.template import RequestContext
+from polls.models import Choice, Poll
 
 def vote(request, poll_id):
-    return HttpResponse("you're voting on poll %s" % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render_to_response('polls/detail.html',
+                                  {'poll': p, 'error_message': "You didn't select a choice."},
+                                  context_instance = RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('poll_results', args=(p.id,)))
